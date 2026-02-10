74007cec6f19db06df23b4d7d4057d9be315f00d
Alicia Wynona Tjahjadi <alicia@intranet.rosadelsvents.es>
2026-02-10 08:21:06 +0100

    Exercise 1 Destructuring

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -1,14 +1,14 @@
 const personOne = {
   name: "Popeye",
   age: 34,
-  favouriteFood: "Spinach",
+  favoriteFood: "Spinach",
 };
 
 // Update the parameter to this function to make it work.
 // Don't change anything else.
-function introduceYourself(___________________________) {
+function introduceYourself({ name, age, favoriteFood }) {
   console.log(
-    `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
+    `Hello, my name is ${name}. I am ${age} years old and my favorite food is ${favoriteFood}.`
   );
 }
 
