bd5f75cddedb9073bd72721c2701cdefea5cb53c
Alicia Wynona Tjahjadi <alicia@intranet.rosadelsvents.es>
2026-02-10 09:20:42 +0100

    Exercise 3 Destructuring

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,20 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+// calculating total price
+// converting to pounds
+
+function printReceipt (orderData) {
+  console.log("QTY".padEnd(6) + "ITEM".padEnd(20) + "TOTAL".padStart(6));
+  for (let {itemName, quantity, unitPricePence} of orderData) {
+    const totalPriceInPounds = (quantity * unitPricePence) / 100;
+    console.log(
+      String(quantity).padEnd(6) +
+      itemName.padEnd(20) +
+      totalPriceInPounds.toFixed(2).padStart(6)
+    );
+  }
+}
+
+printReceipt(order);
